e1055be55a3a9bb8c85cfc47b9b1302bcb0f1108
Claudio Bardales <claudioeduardobardales@gmail.com>
2022-11-15 21:44:04 -0800

    Setting up react manually

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { hot } from 'react-hot-loader/root';
+import React from 'react';
+import 'App.css';
+const App = () => (
+  <div>
+    <h1>Is this finally working?</h1>
+  </div>
+);
+
+export default hot(App);

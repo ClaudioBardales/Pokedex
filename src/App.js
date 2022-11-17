505ce42d2516734aa18e8925db61f602a520d0ea
Claudio Bardales <claudioeduardobardales@gmail.com>
2022-11-16 20:34:43 -0800

    Adding styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import PokeApi from './PokeApi';
-import 'App.css';
+import GlobalStyle from './globalStyles';
 const App = () => (
   <>
+    <GlobalStyle />
     <PokeApi />
   </>
 );
--- src/PokeApi.js
+++ src/PokeApi.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-function PokeApi() {
+import styled from 'styled-components';
+
+const PokeApi = () => {
   const [name, setName] = useState('');
   const [find, setFind] = useState('pikachu');
   const [img, setImg] = useState('');
@@ -26,18 +28,50 @@ function PokeApi() {
   };
 
   return (
-    <>
-      <div className="back">
-        <div className="card">
-          <img src={img} alt="" />
-          <div className="name">{find.toUpperCase()}</div>
-          <div className="type">{type}</div>
-          <input type="text" onChange={Typename} value={name} />
-          <button onClick={Search}>Search</button>
-        </div>
-      </div>
-    </>
+    <Container>
+      <Wrapper>
+        <img src={img} alt="" />
+        <div>{find.toUpperCase()}</div>
+        <div>{type}</div>
+        <input type="text" onChange={Typename} value={name} />
+        <Button onClick={Search}>Search</Button>
+      </Wrapper>
+    </Container>
   );
-}
+};
+
+const Container = styled.div`
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Wrapper = styled.div`
+  border: 5px solid #ffde00;
+  background-color: #3b4cca;
+  display: flex;
+  flex-direction: column;
+  width: 30%;
+  border-radius: 25px;
+  box-shadow: 2.8px 2.8px 2.2px rgba(0, 0, 0, 0.02),
+    6.7px 6.7px 5.3px rgba(0, 0, 0, 0.028),
+    12.5px 12.5px 10px rgba(0, 0, 0, 0.035),
+    22.3px 22.3px 17.9px rgba(0, 0, 0, 0.042),
+    41.8px 41.8px 33.4px rgba(0, 0, 0, 0.05),
+    100px 100px 80px rgba(0, 0, 0, 0.07);
+`;
+
+const Button = styled.div`
+  border: none;
+  text-align: center;
+  padding: 10px;
+  background-color: #ff0000;
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+  font-weight: bold;
+  color: white;
+  cursor: pointer;
+`;
 
 export default PokeApi;
